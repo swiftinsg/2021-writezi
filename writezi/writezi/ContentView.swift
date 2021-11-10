e6cd1cf0c16b21b03cb7413a521406e522aca499
Ang Jun Ray <angjunray@live.com>
2021-11-10 15:17:12 +0800

    Xcode Initial Project!

---
+++ writezi/writezi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  writezi
+//
+//  Created by Ang Jun Ray on 10/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ writezi/writezi/writeziApp.swift
@@ -0,0 +1,17 @@
+//
+//  writeziApp.swift
+//  writezi
+//
+//  Created by Ang Jun Ray on 10/11/21.
+//
+
+import SwiftUI
+
+@main
+struct writeziApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
