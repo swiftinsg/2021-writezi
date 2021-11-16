caceaaf9c92d9436ebace476a00fd1e30dc886de
neohaojun <neohaojun@gmail.com>
2021-11-16 15:12:20 +0800

    Changed the New spelling view

--- writezi/writezi/NewSpellingView.swift
+++ writezi/writezi/NewSpellingView.swift
@@ -2,14 +2,37 @@
 //  NewSpellingView.swift
 //  writezi
 //
-//  Created by hao jun on 16/11/21.
+//  Created by jun hao on 16/11/21.
 //
 
 import SwiftUI
 
 struct NewSpellingView: View {
+    @State private var newSpellingList = SpellingList(name: "")
     var body: some View {
-        Text("Hello, World!")
+        NavigationView{
+            Form{
+                Section (header: Text("Title")){
+                    TextField("Title of Spelling List", text: $newSpellingList.name )
+                }
+            
+                Section (header: HStack{
+                    Text("Words")
+                    Spacer()
+                    Button{
+                        newSpellingList.spellingList.append("")
+                    } label : {
+                        Image(systemName: "plus")
+                    }
+                    
+                }){
+                    ForEach($newSpellingList.spellingList, id: \.self) { $spellingList in
+                        TextField( "Word", text: $spellingList)
+                    }
+                }
+            }
+            .navigationTitle("New Spelling List")
+        }
     }
 }
 
