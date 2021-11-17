b0bbe96d65659977937a8ab9ff2d2ff8527974dd
neohaojun <85774385+JunHao28@users.noreply.github.com>
2021-11-17 19:06:28 +0800

    Did CheckAnswerView layout

--- writezi/writezi/CheckAnswerView.swift
+++ writezi/writezi/CheckAnswerView.swift
@@ -8,8 +8,69 @@
 import SwiftUI
 
 struct CheckAnswerView: View {
+    
+    @State private var quit = false
+    @State private var exit = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack{
+                NavigationLink(destination: ContentView().navigationBarHidden(true), isActive: self.$exit) { EmptyView() }
+                Spacer()
+                Text("Hello")
+                Spacer()
+                HStack{
+                    Button{
+                        
+                    } label: {
+                        Text("Wrong")
+                            .frame(minWidth: 0, maxWidth: .infinity)
+                    }
+                    .tint(Color("Danger"))
+                    .frame(width: UIScreen.main.bounds.size.width * 0.35)
+                    .padding()
+                    .buttonStyle(.bordered)
+                    .controlSize(.large)
+                    Button{
+                        
+                    } label: {
+                        Text("Correct")
+                            .frame(minWidth: 0, maxWidth: .infinity)
+                    }
+                    .tint(Color("Success"))
+                    .frame(width: UIScreen.main.bounds.size.width * 0.35)
+                    .padding()
+                    .buttonStyle(.bordered)
+                    .controlSize(.large)
+                }
+            }
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+            .toolbar{
+                ToolbarItem(placement: .navigationBarLeading, content: {
+                    Button {
+                        quit = true
+                    }label: {
+                        Image(systemName: "xmark")
+                    }
+                })
+            }
+            .alert(isPresented: $quit) {
+                Alert(
+                    title: Text("Are you sure you want to quit?"),
+                    message: Text("All your data for this spelling test will be lost. "),
+                    primaryButton: .destructive(
+                        Text("Quit"),
+                        action: {
+                            exit = true
+                        }
+                    ), secondaryButton: .default(
+                        Text("Cancel"),
+                        action: {}//No Action to be done
+                    )
+                )
+            }
+            .background(Color(.systemGroupedBackground))
+        }
     }
 }
 
