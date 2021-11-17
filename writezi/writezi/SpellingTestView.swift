4553ae72317d48c3de22391c7fcd81a8c4151b9d
neohaojun <85774385+JunHao28@users.noreply.github.com>
2021-11-17 16:47:58 +0800

    Added a timer mode

--- writezi/writezi/SpellingTestView.swift
+++ writezi/writezi/SpellingTestView.swift
@@ -16,14 +16,41 @@ struct SpellingTestView: View {
     @State private var quit = false;
     @State private var exit = false;
     @State private var questionNo = 0
+    @State private var timeRemaining = 30
+    
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View {
         VStack{
             Spacer()
             NavigationLink(destination: CheckAnswerView().navigationBarHidden(true), isActive: self.$finish) { EmptyView() }
             NavigationLink(destination: ContentView().navigationBarHidden(true), isActive: self.$exit) { EmptyView() }
+            if spellingMode == 1 {
+                ZStack {
+                    Circle()
+                        .fill(Color.white)
+                        .frame(width: 100.0, height: 100.0)
+                        
+                    Text("\(timeRemaining)")
+                        .font(.largeTitle)
+                        .cornerRadius(100.0)
+                        .onReceive(timer) { time in
+                            if self.timeRemaining > 0 {
+                                self.timeRemaining -= 1
+                            } else {
+                                if questionNo == spellingList.spellingList.count - 1 {
+                                    finish = true
+                                } else {
+                                    timeRemaining = 30
+                                    questionNo += 1
+                                }
+                            }
+                        }
+                    .padding()
+                }
+            }
             Button{
-                
+                // Sound
             } label: {
                 Image(systemName: "speaker.wave.3.fill")
                     .imageScale(.large)
@@ -36,7 +63,7 @@ struct SpellingTestView: View {
             }
             Spacer()
             HStack{
-                if questionNo > 0 {
+                if questionNo > 0 && spellingMode != 1{
                     Button{
                         questionNo -= 1
                     } label: {
@@ -53,13 +80,14 @@ struct SpellingTestView: View {
                     if questionNo == spellingList.spellingList.count - 1 {
                         finish = true
                     } else {
+                        timeRemaining = 30
                         questionNo += 1
                     }
                     
                 } label: {
                     if questionNo == spellingList.spellingList.count - 1 {
-                    Text("Finish")
-                        .frame(minWidth: 0, maxWidth: .infinity)
+                        Text("Finish")
+                            .frame(minWidth: 0, maxWidth: .infinity)
                     } else {
                         Text("Next")
                             .frame(minWidth: 0, maxWidth: .infinity)
@@ -72,6 +100,7 @@ struct SpellingTestView: View {
                 .controlSize(.large)
             }
         }
+        .navigationTitle("Question \(questionNo + 1)")
         .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
         .toolbar{
             ToolbarItem(placement: .navigationBarLeading, content: {
