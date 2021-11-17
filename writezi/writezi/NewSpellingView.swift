5279dac5374f8c05162e6ce49abf14885eaf2530
Ang Jun Ray <angjunray@live.com>
2021-11-17 16:50:25 +0800

    detecting the language...done!

--- writezi/writezi/NewSpellingView.swift
+++ writezi/writezi/NewSpellingView.swift
@@ -8,12 +8,22 @@
 import SwiftUI
 import NaturalLanguage
 
-func detectedLanguage(for string: String) -> String? {
-    let recognizer = NLLanguageRecognizer()
-    recognizer.processString(string)
-    guard let languageCode = recognizer.dominantLanguage?.rawValue else { return nil }
+struct LanguageDetails{
+    var confidence: [String: Double]
+    var detectedLanguage: String?
+}
+
+func detectedLanguage(for string: String) -> LanguageDetails? {
+    let languageRecognizer = NLLanguageRecognizer()
+    languageRecognizer.processString(string)
+    guard let languageCode = languageRecognizer.dominantLanguage?.rawValue else { return nil }
     let detectedLanguage = Locale.current.localizedString(forIdentifier: languageCode)
-    return detectedLanguage
+    let confidence = languageRecognizer.languageHypotheses(withMaximum: 100)
+    var newConfidence = [String: Double]()
+    for i in confidence{
+        newConfidence[Locale.current.localizedString(forIdentifier: i.key.rawValue)!] = i.value
+    }
+    return LanguageDetails(confidence: newConfidence, detectedLanguage: detectedLanguage)
 }
 
 struct NewSpellingView: View {
@@ -50,22 +60,41 @@ struct NewSpellingView: View {
                 Button {
                     //validate the list
                     for i in 0..<newSpellingList.spellingList.count{
+                        
+                        //check if the word is empty
                         if(newSpellingList.spellingList[i].word == ""){
                             //Alert
                             alertToShow = "The \(i+1)th word is empty!"
                             alertPresented = true
                             return
                         }
+                        
                         let detectedLang = detectedLanguage(for: newSpellingList.spellingList[i].word)
-                        if(detectedLang != "Chinese, Traditional" && detectedLang != "Chinese, Simplified"){
-                            alertToShow = "The \(i+1)th word is not Chinese, its in the \(String(describing: detectedLang == nil ? "Unidentified" : detectedLang!)) language!"
+                        //check if natural language can find out whats the lang
+                        
+                        if(detectedLang == nil){
+                            alertToShow = "The \(i+1)th word's language cannot be detected!"
+                            alertPresented = true
+                            return
+                        }
+                        
+                        //check the language itself of each word
+                        if(detectedLang!.detectedLanguage! != "Chinese, Traditional" && detectedLang!.detectedLanguage! != "Chinese, Simplified"){
+                            alertToShow = "The \(i+1)th word is not Chinese, its in the \(detectedLang!.detectedLanguage!)) language!"
+                            alertPresented = true
+                            return
+                        }
+                        
+                        //check the confidence of detection
+                        if(detectedLang?.confidence["Chinese, Simplified"] > 95 && detectedLang?.confidence["Chinese, Traditional"] > 95){
+                            alertToShow = "The \(i+1)th word is not 100% Chinese!"
                             alertPresented = true
                             return
                         }
                     }
                     let detectedLang = detectedLanguage(for: newSpellingList.name)
-                    if(detectedLang != "Chinese, Traditional" && detectedLang != "Chinese, Simplified" && detectedLang != "English"){
-                        alertToShow = "The title is not Chinese or English, its in the \(String(describing: detectedLang == nil ? "Unidentified" : detectedLang!)) language!"
+                    if(detectedLang!.detectedLanguage! != "Chinese, Traditional" && detectedLang!.detectedLanguage! != "Chinese, Simplified" && detectedLang!.detectedLanguage! != "English"){
+                        alertToShow = "The title is not Chinese or English, its in the \(detectedLang!.detectedLanguage!)) language!"
                         alertPresented = true
                         return
                     }
@@ -77,6 +106,14 @@ struct NewSpellingView: View {
                         alertToShow = "Spelling List Title Cannot Be Empty!"
                         alertPresented = true
                     }
+                    
+                    //check the confidence of detection
+                    if(detectedLang?.confidence["Chinese, Simplified"]? > 95 && detectedLang?.confidence["Chinese, Traditional"] > 95 && detectedLang?.confidence["English"] > 95){
+                        alertToShow = "The title is not 100% Chinese!"
+                        alertPresented = true
+                        return
+                    }
+                    
                 } label: {
                     Text("Save")
                 }
